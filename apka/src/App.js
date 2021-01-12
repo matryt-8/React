85b81eb6f100f883276589b8b6f54f99d7e8cbbd
Mateusz Rytwiński <mateuszrytwinski@onet.pl>
2021-01-12 14:34:19 +0100

    added some framework

--- apka/src/App.js
+++ apka/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+axios.defaults.baseURL = "http://localhost:7777/";
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        {/* <nav>Navbar</nav> */}
+        <Router>
+          <Route exact path="/" render={() => <h1 className="App">Home</h1>} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
