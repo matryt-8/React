744bde9247f1c31d02d8e9f20abfeb65260af78f
Mateusz Rytwiński <mateuszrytwinski@onet.pl>
2021-01-12 14:55:38 +0100

    added some basic methods for axios

--- apka/src/App.js
+++ apka/src/App.js
@@ -12,6 +12,71 @@ class App extends Component{
     };
   }
 
+  addMovie = (data) =>{
+    var body = {
+      "id": data.id,
+      "title": data.title,
+      "duration": data.duration
+    }
+
+    axios.post("movies", body)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }
+
+  editMovie = (data) =>{
+    var body = {
+      "id": data.id,
+      "title": data.title,
+      "duration": data.duration
+    }
+
+    axios.put("movies/" + body.id, body)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }
+
+  deleteMovie = (id) =>{
+    
+    axios.delete("movies/"+id)
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+  }
+
+  addScreening = (data) =>{
+    var body = {
+      "id": data.id,
+      "date": data.date,
+      "hour": data.hour,
+      "movie": data.movie,
+      "room": data.room,
+      "sold tickets": data.soldtickets,
+      "free tickets": data.freetickets,
+      "taken seats": data.takenseats
+    }
+
+    axios.post("screenings", body)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }
+
+  editScreening = (data) =>{
+    var body = {
+      "id": data.id,
+      "date": data.date,
+      "hour": data.hour,
+      "movie": data.movie,
+      "room": data.room,
+      "sold tickets": data.soldtickets,
+      "free tickets": data.freetickets,
+      "taken seats": data.takenseats
+    }
+
+    axios.put("screenings"+ body.id, body)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }
+
   render(){
     return (
       <div>
